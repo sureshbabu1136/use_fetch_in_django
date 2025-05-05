c777b9f93a2c298e8bcd60e7169a1c6b63be1b7a
sureshbabu1136 <sureshbabuthangaraj1136@gmail.com>
2025-05-05 18:31:19 +0530

    main:sample_try

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,5 +4,5 @@ from myapp import views
 
 urlpatterns = [
     path("",views.index,name="index"),
-    path("/form_data",views.form_data,name="form_data")
+    path("form_data",views.form_data,name="form_data")
 ]
