e01ffb1a6e5840b226663d98a9492db50b46196d
sureshbabu1136 <sureshbabuthangaraj1136@gmail.com>
2025-05-05 18:23:01 +0530

    main:assign functions

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,4 +4,5 @@ from myapp import views
 
 urlpatterns = [
     path("",views.index,name="index"),
+    path("/form_data",views.form_data,name="form_data")
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -6,3 +6,10 @@ from rest_framework.decorators import api_view
 @api_view(["GET"])
 def index(request):
     return render(request,"index.html")
+
+@api_view(["POST","GET"])
+def form_data(request):
+    if request.method == "POST":
+        username=request.data.get("username")
+        password=request.data.get("password")
+        return response({"message":username})
